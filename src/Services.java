af14b65fba369ea0a2eb1dd0225ce3dccbf6bece
zyx <wjszzyx@sohu.com>
2017-05-16 20:06:10 +0800

    flight reservation finished?

--- src/Services.java
+++ src/Services.java
@@ -1,3 +1,5 @@
+import java.sql.*;
+
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
@@ -7,8 +9,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneLayout;
+import javax.swing.JTable;
+import javax.swing.table.*;
 
 public class Services extends JFrame implements ActionListener {
 	private Container cp;
@@ -19,23 +28,188 @@ public class Services extends JFrame implements ActionListener {
 	private int serviceID;
 	
 	public class Flight extends JPanel implements ActionListener {
+		private String custName;
 		
+		private JPanel flightSearchP;
+		private JLabel fromCityL, arivCityL;
+		private JTextField fromCityTF, arivCityTF;
+		private JButton searchB;
+		private JPanel flightResultsP;
+		private JScrollPane resultsSP;
+		private JTable resultsT;
+		private DefaultTableModel resultsTM;
+		private JButton reserveB;
+		
+		public Flight(String custName) {
+			super(new FlowLayout(FlowLayout.CENTER, 10, 10));
+			this.custName = custName; 
+			setFlightWindow();
+		}
+		
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource() == searchB) {
+				search();
+			}
+			if(e.getSource() == reserveB) {
+				reserve();
+			}
+		}
+
+		public void setFlightWindow() {
+			flightSearchP = new JPanel(new GridLayout(5, 1, 0, 0));
+			flightSearchP.setPreferredSize(new Dimension(300, 200));
+			fromCityL = new JLabel("始发地");
+			fromCityTF = new JTextField("");
+			arivCityL = new JLabel("目的地");
+			arivCityTF = new JTextField("");
+			searchB = new JButton("查询");
+			searchB.addActionListener(this);
+			flightSearchP.add(fromCityL);
+			flightSearchP.add(fromCityTF);
+			flightSearchP.add(arivCityL);
+			flightSearchP.add(arivCityTF);
+			flightSearchP.add(searchB);
+			flightSearchP.setVisible(true);
+			
+			resultsTM = new DefaultTableModel();
+			resultsTM.setColumnIdentifiers(new String[] {"航班号", "价格", "座位数", "剩余座位数", "始发地", "目的地"});
+			resultsT = new JTable(resultsTM) {
+				public boolean isCellEditable(int row, int column) {
+					return false;
+				}
+			};
+			resultsT.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+			resultsT.setFillsViewportHeight(true);
+			resultsSP = new JScrollPane();
+			resultsSP.setLayout(new ScrollPaneLayout());
+			resultsSP.setPreferredSize(new Dimension(500, 300));
+			resultsSP.setViewportView(resultsT);
+			reserveB = new JButton("预订");
+			reserveB.addActionListener(this);
+			reserveB.setEnabled(false);
+			flightResultsP = new JPanel();
+			flightResultsP.setPreferredSize(new Dimension(550, 350));
+			flightResultsP.add(resultsSP);
+			flightResultsP.add(reserveB);
+			
+			add(flightSearchP);
+			add(flightResultsP);
+			setVisible(true);
+			setPreferredSize(new Dimension(800, 800));
+		}
+		
+		public void search() {
+			String flightNum = null, price = null, numSeats = null, numAvail = null;
+			String fromCity = fromCityTF.getText();
+			String arivCity = arivCityTF.getText();
+			Connection conn = DBInterface.getConnection();
+			ResultSet rs = null;
+			
+			if(fromCity.matches("") || arivCity.matches("")) {
+				JOptionPane.showMessageDialog(null, "出发地和目的地不能为空", "没找到航班", JOptionPane.ERROR_MESSAGE);
+			}
+			else {
+				String query = "select * from FLIGHTS where fromCity = '" + fromCity + "' and arivCity = '" + arivCity + "';";
+				try {
+					rs = DBInterface.executeQuery(conn, query);
+					while(rs.next()) {
+						flightNum = rs.getString("flightNum");
+						price = rs.getString("price");
+						numSeats = rs.getString("numSeats");
+						numAvail = rs.getString("numAvail");
+						if(Integer.parseInt(numAvail) > 0) {
+							resultsTM.addRow(new Object[] {flightNum, price, numSeats, numAvail, fromCity, arivCity});
+						}
+					}
+				} catch(SQLException e) {
+					System.out.println("查询航班时数据库操作错误。");
+					e.printStackTrace();
+				}
+			}
+			DBInterface.closeConnection(conn);
+			//resultsT.setModel(resultsTM);
+			//resultsT.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+			//resultsT.setFillsViewportHeight(true);
+			//flightResultsP.add(scrollPane);
+			flightResultsP.setVisible(true);
+			reserveB.setEnabled(true);
+		}
+		
+		public void reserve() {
+			int i = resultsT.getSelectedRow();
+			if(i != -1) {
+				Object _flightNum = resultsT.getValueAt(i, 0);
+				//Object _price = resultsT.getValueAt(i, 1);
+				//Object _numSeats = resultsT.getValueAt(i, 2);
+				Object _numAvail = resultsT.getValueAt(i, 3);
+				//Object _fromCity = resultsT.getValueAt(i, 4);
+				//Object _arivCity = resultsT.getValueAt(i, 5);
+				String flightNum = String.valueOf(_flightNum);
+				int numAvail = Integer.parseInt(String.valueOf(_numAvail));
+				
+				Connection conn = DBInterface.getConnection();
+				String insert, update;
+				long timeStamp = new java.util.Date().getTime();
+				insert = "insert into RESERVATIONS (custName, resvType, resvKey) values ('" + custName + "', 1, '" + custName + timeStamp + "');";
+				update = "update FLIGHTS set numAvail = " + (numAvail - 1) + " where flightNum = '" + flightNum + "';";
+				try {
+					conn.setAutoCommit(false);
+					DBInterface.executeUpdate(conn, insert);
+					DBInterface.executeUpdate(conn, update);
+					conn.commit();
+					conn.setAutoCommit(true);
+				} catch(SQLException e) {
+					System.out.println("订购机票时数据库操作错误。");
+					e.printStackTrace();
+					return;
+				}
+				JOptionPane.showMessageDialog(null, "订票成功", "啦啦啦", JOptionPane.WARNING_MESSAGE);
+				DBInterface.closeConnection(conn);
+				fromCityTF.setText("");
+				arivCityTF.setText("");
+				resultsTM.setRowCount(0);
+				reserveB.setEnabled(false);
+			}
+		}
 	}
 	
 	public class Hotel extends JPanel implements ActionListener {
+		public Hotel(String custName) {
+			
+		}
 		
+		public void actionPerformed(ActionEvent e) {
+			
+		}
 	}
 	
 	public class Car extends JPanel implements ActionListener {
+		public Car(String custName) {
+			
+		}
 		
+		public void actionPerformed(ActionEvent e) {
+			
+		}
 	}
 	
 	public class Reservation extends JPanel implements ActionListener {
-		
+		public void actionPerformed(ActionEvent e) {
+			
+		}
 	}
+	
+	Flight flightP;
+	Hotel hotelP;
+	Car carP;
+	Reservation reservationP;
 	
 	public Services(String userName) {
 		super("请选择服务");
+		flightP = new Flight(userName);
+		hotelP = new Hotel(userName);
+		carP = new Car(userName);
+		reservationP = new Reservation();
 		setWindow();
 	}
 	
@@ -69,10 +243,10 @@ public class Services extends JFrame implements ActionListener {
 		serviceCP = new JPanel();
 		card = new CardLayout();
 		serviceCP.setLayout(card);
-		serviceCP.add("0", flight);
-		serviceCP.add("1", hotel);
-		serviceCP.add("2", car);
-		serviceCP.add("3", reservation);
+		serviceCP.add("0", flightP);
+		serviceCP.add("1", hotelP);
+		serviceCP.add("2", carP);
+		serviceCP.add("3", reservationP);
 		serviceCP.setPreferredSize(new Dimension(600, 600));
 		serviceCP.setVisible(true);
 		
